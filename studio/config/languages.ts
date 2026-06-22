ecd3f1f31b90ea99d27b9357f13f6ceb06a9f317
Evelyn <tuletuuyen@gmail.com>
2026-06-22 15:37:41 +0800

    Update languages.ts
    Add Viet sub

--- studio/config/languages.ts
+++ studio/config/languages.ts
@@ -2,4 +2,5 @@ export const STUDIO_LANGUAGES = [
   { title: 'English', value: 'en' },
   { title: 'Español', value: 'es' },
   { title: 'Bahasa Indonesia', value: 'id' },
+  { title: 'Tiếng Việt', value: 'vi' },
 ] as const;
